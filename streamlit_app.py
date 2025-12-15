3ff1ed54842aa29011506916c73c935f45a4115f
VFjoaopinto <139543824+VFjoaopinto@users.noreply.github.com>
2025-12-15 12:40:54 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,8 +8,9 @@ st.title(":cup_with_straw: Customize Your Smoothie! :cup_with_straw:")
 
 cnx = st.connection("snowflake")
 session = cnx.session()
-my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
-#st.dataframe(data=my_dataframe, use_container_width=True)
+my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'), col('SEARCH_ON'))
+st.dataframe(data=my_dataframe, use_container_width=True)
+st.stop()
 
 ingredients_list = st.multiselect('Choose up to 5 ingridients:', my_dataframe, max_selections=5)
 
